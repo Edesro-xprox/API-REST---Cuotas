a87f9a6e2d2cae67ed0d0beca45ff792fced1625
Edson Espinoza <edesro1996@gmail.com>
2025-01-03 00:21:58 -0500

    Agregado de CORS

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
 
 //Configuración
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 
